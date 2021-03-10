b58fcaf21ccf6b4dde84ebd29765a9dc4b535835
Sirwel <lewrismota@hotmail.com>
2021-03-10 17:49:50 -0500

    ADP Test Initial Commit

---
+++ ADPCashManager/src/main/java/com/adp/demo/AdpCashManagerApplication.java
@@ -0,0 +1,13 @@
+package com.adp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpCashManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpCashManagerApplication.class, args);
+	}
+
+}
